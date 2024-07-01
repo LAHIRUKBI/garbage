07fb1ffcd1af6d4c83bb5d75c064259acb06df65
Lahiru <lahiruilangasinha@gmail.com>
2024-07-02 00:02:27 +0530

    create and add OAuth component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "garbage-23513.firebaseapp.com",
+  projectId: "garbage-23513",
+  storageBucket: "garbage-23513.appspot.com",
+  messagingSenderId: "753400207125",
+  appId: "1:753400207125:web:705b22f27fc4936996bb1c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
