f290c369b0d88eb4b998982b53d0c9caee51080a
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2024-07-09 19:41:22 +0530

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -188,6 +188,29 @@ const handleSignOut = async () => {
 };
 
 
+
+
+
+ // Function to handle delete the list
+const handleListingDelete = async (listingId) => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      console.log(data.message);
+      return;
+    }
+
+    setUserListings((prev) =>
+      prev.filter((listing) => listing._id !== listingId)
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
   
   return (
     <div className="p-3    max-w-lg  mx-auto">
