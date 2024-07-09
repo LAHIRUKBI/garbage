fa28c066332a313ae4d7df3bf70974827b3cb5f2
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2024-07-09 12:41:26 +0530

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
 
-router.get('/test', test);
+router.get('/test', test);                                  //Defines a GET route at the path /test that calls the test function when accessed.
 router.post('/update/:id', verifyToken, updateUser)
-router.delete('/delete/:id', verifyToken, deleteUser)    
+router.delete('/delete/:id', verifyToken, deleteUser)  
+router.get('/listings/:id', verifyToken, getUserListings)  
 
 export default router;
